9be9384b82af8ef6c251b5832963133772a07c65
William Wu <williamwu277@gmail.com>
2025-01-09 12:15:52 -0500

    Update 2

--- Backend/main.py
+++ Backend/main.py
@@ -106,5 +106,5 @@ def get_graphs(query: UserQuery):
     return ret["Description"]
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
